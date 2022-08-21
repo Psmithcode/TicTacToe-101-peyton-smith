440c26a783b54ca6aa2ff540cab57d97b3be1200
Psmithcode <peytonsmith53@gmail.com>
2022-08-21 17:53:00 -0500

    handleClick is not working

--- scripts.js
+++ scripts.js
@@ -1,25 +1,16 @@
-let currentMarker = 'X'
-
-
-
-const board = [
+let board = [
    ["", "", ""],
    ["", "", ""],
    ["", "", ""]
 ]
 
-board[0][0] = document.getElementById("0-0")
-
-
-const row = parseInt(element.id.charAt(0))
-const column = parseInt(element.id.charAt(2))
-
+let currentMarker = 'X'
 
 const handleClick = (element) => {
-
+  
   
   console.log(`The element you clicked on has an id:  ${element.id}`)
-
+  
   if(!document.getElementById(element.id).innerHTML){
     addMarker(element.id)
   }
@@ -28,16 +19,20 @@ const handleClick = (element) => {
 
 
 const addMarker = (id) => {
-
+  
+  const row = parseInt(element.id.charAt(0))
+  const column = parseInt(element.id.charAt(2))
   
   console.log(`*** The current marker is:  ${currentMarker}. ***`)
   console.log(`Therefore, a  "${currentMarker}"  should be placed in the square with the id:  ${id}`)
   
   
   document.getElementById(id).innerHTML = currentMarker
-  board[row][column] = currentMarker
 
-  changeMarker()
+
+  board[row][column] = currentMarker
+  console.log(board)
+  checkForWin()
 }
 
 const changeMarker = () => {
@@ -48,11 +43,11 @@ const changeMarker = () => {
   }
 }
 
-
+// maybe remove the button
 const resetBoard = (button) => {
   
   
-    const squares = document.getElementsByTagName('td') 
+    const squares = document.getElementsByTagName('TD') 
   
   for (i=0; i < squares.length; i++) {
 
@@ -61,4 +56,47 @@ const resetBoard = (button) => {
 
     squares[i].innerHTML = null
   }  
+}
+
+
+const checkForWin = () => {
+  if(horizontalWin() || verticalWin() || diagonalWin()) {
+    window.alert(`Player ${currentMarker} won!`)
+  } else {
+    changeMarker()
+  }
+}
+
+const horizontalWin = () => {
+  if((board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") 
+  || (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O")
+  || (board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X")
+  || (board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O")
+  || (board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X")
+  || (board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O")
+){
+  return true
+}
+}
+
+const verticalWin = () => {
+  if((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") 
+  || (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")
+  || (board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X")
+  || (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O")
+  || (board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X")
+  || (board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O")
+){
+  return true
+}
+}
+
+const diagonalWin = () => {
+  if((board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") 
+  || (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O")
+  ||(board[0][2] == "X" && board[1][1] == "X" && board[2][0] == "X") 
+  || (board[0][2] == "O" && board[1][1] == "O" && board[2][0] == "O")
+){
+   return true
+}   
 }
